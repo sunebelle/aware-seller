88b1f6c6d3a3956255e816d70e0cc10ca1021170
Suneflower <nguyenbinh2896@gmail.com>
2021-10-11 17:21:07 +0700

    Initialized aware project

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DB = process.env.DB_URL;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connect successfully"));
+
+const port = 5000 || process.env.PORT;
+
+app.listen(port, () =>
+  console.log(`server is listening on port ${port}, ${__dirname}`)
+);
+
